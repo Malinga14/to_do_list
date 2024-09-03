885ef6682ad01fdabd34bea54902fe724004d3d3
Malinga Pasindu <malingapasindu14@gmail.com>
2024-09-03 11:23:00 +0530

    6th Commit

--- src/main/java/controller/CompletedTaskView.java
+++ src/main/java/controller/CompletedTaskView.java
@@ -3,25 +3,17 @@ package controller;
 import db.DBConnection;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Stage;
+
 import model.Record;
 
-import java.awt.*;
-import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+
 import java.util.ResourceBundle;
 
 public class CompletedTaskView implements Initializable {
